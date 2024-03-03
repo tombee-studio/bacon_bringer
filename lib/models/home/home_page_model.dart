4af44a314fae37e91603eae3acc602096e31f607
tombee-creator <tomomail1997@gmail.c.om>
2024-03-03 10:54:25 +0900

    titleにgetterを追加

--- lib/models/home/home_page_model.dart
+++ lib/models/home/home_page_model.dart
@@ -2,12 +2,13 @@ import 'package:bacon_bringer/bases/model.dart';
 import 'package:bacon_bringer/bases/property.dart';
 
 class HomePageModel extends Model {
-  final String title;
+  final String _title;
   late Property<int> _counter;
 
+  String get title => _title;
   String get counter => '${_counter.value}';
 
-  HomePageModel(super.notifier, this.title) {
+  HomePageModel(super.notifier, this._title) {
     _counter = propertyOf(0);
   }
 
