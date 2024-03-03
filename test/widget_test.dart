ce795ba0c961994023ea7fb8cd9f690c366158b0
tombee-creator <tomomail1997@gmail.c.om>
2024-03-03 11:42:20 +0900

    テストケースの追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,44 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:bacon_bringer/ui/home/view_model/home_page_view_model.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bacon_bringer/main.dart';
 
-import 'util.dart';
-
 void main() {
-  test('Counter increment on home page', () {
-    const testTitle = "title";
-    final HomePageViewModel viewModel =
-        HomePageViewModel(TestNotifier(), testTitle);
-    expect(viewModel.counter, '0');
-    viewModel.incrementCounter();
-    expect(viewModel.counter, '1');
-  });
-
-  testWidgets('can get title on home page', (WidgetTester tester) async {
+  testWidgets('ホーム画面でタイトルが表示されていること', (WidgetTester tester) async {
     const testTitle = "Bacon Bringer";
     await tester.pumpWidget(const BaconBringerApp());
     expect(find.text(testTitle), findsOneWidget);
   });
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('ホーム画面で月間収支概要が表示されていること', (WidgetTester tester) async {
     await tester.pumpWidget(const BaconBringerApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("合計"), findsOneWidget);
+    expect(find.text("100000"), findsOneWidget);
   });
 }
