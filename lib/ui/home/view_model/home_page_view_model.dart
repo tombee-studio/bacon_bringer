5a0d5f9e8db7f44adcb2ac92b6a5f749e3c2caca
tombee-creator <tomomail1997@gmail.c.om>
2024-03-03 19:29:04 +0900

    ローディング、収支概要コンポーネントを使用

--- lib/ui/home/view_model/home_page_view_model.dart
+++ lib/ui/home/view_model/home_page_view_model.dart
@@ -2,6 +2,8 @@ import 'package:bacon_bringer/bases/notifier.dart';
 import 'package:bacon_bringer/bases/view_model.dart';
 import 'package:bacon_bringer/model/home/home_page_model.dart';
 import 'package:bacon_bringer/model/home/repository/home_page_app_repository.dart';
+import 'package:bacon_bringer/ui/common/loading_component.dart';
+import 'package:bacon_bringer/ui/home/view/overview_component.dart';
 import 'package:flutter/material.dart';
 
 class HomePageViewModel extends ViewModel<HomePageModel> {
@@ -20,31 +22,25 @@ class HomePageViewModel extends ViewModel<HomePageModel> {
   Widget get title => Text(model.title);
   Widget get overview {
     final overviewData = model.overviewData;
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(children: [
-          Row(children: [const Text("合計"), Text('${overviewData.sumOfMoney}')])
-        ]));
+    return OverviewComponent(overviewData: overviewData);
   }
 
   Widget body(BuildContext context) {
     final loadingData = model.isLoading;
     if (loadingData.isLoading) {
-      return SizedBox(
-          width: 200,
-          height: 200,
-          child: Column(children: [
-            const CircularProgressIndicator(),
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Text(loadingData.message))
-          ]));
+      return LoadingComponent(loadingData: loadingData);
     } else {
       final size = MediaQuery.of(context).size;
       if (size.width > 600) {
-        return SizedBox(width: 600, child: overview);
+        return SizedBox(
+            width: 600,
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [overview, const Spacer()])));
       } else {
-        return overview;
+        return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: [overview, const Spacer()]));
       }
     }
   }
