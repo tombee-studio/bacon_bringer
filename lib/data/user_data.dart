22b3d4c71345c7f7042e277dbea44828e2f26b6d
tombee-creator <tomomail1997@gmail.c.om>
2024-03-06 21:42:12 +0900

    不要なプロパティを修正

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,14 +1,7 @@
-import 'package:bacon_bringer/data/account_data.dart';
-
 class UserData {
   final String id;
   final String userName;
   final String password;
-  final List<AccountData> accounts;
 
-  UserData(
-      {required this.id,
-      required this.userName,
-      required this.password,
-      required this.accounts});
+  UserData({required this.id, required this.userName, required this.password});
 }
