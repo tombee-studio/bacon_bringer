66bb15ddc3d9993b6bffcc7765ea45b06de1ee8f
tombee-creator <tomomail1997@gmail.c.om>
2024-03-10 03:49:52 +0900

    テストにgroupを追加

--- test/account_screen_test.dart
+++ test/account_screen_test.dart
@@ -19,209 +19,214 @@ class AccountScreenTestRepository extends AccountScreenRepository {
 }
 
 void main() {
-  // [テスト] AccountScreenAppRepository
-  test("AccountScreenAppRepositoryのaddAccountがAccountDataを作成すること", () async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    const testAccountName = "test_account_name_created";
-    const testAccountPurpose = "test_account_purpose_created";
-    final repository = AccountScreenAppRepository();
-    final account =
-        await repository.addAccount(user, testAccountName, testAccountPurpose);
-    expect(account.user.id, user.id);
-    expect(account.user.userName, user.userName);
-    expect(account.user.password, user.password);
-    expect(account.name, testAccountName);
-    expect(account.purpose, testAccountPurpose);
+  group("AccountScreenAppRepository", () {
+    test("AccountScreenAppRepositoryのaddAccountがAccountDataを作成すること", () async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      const testAccountName = "test_account_name_created";
+      const testAccountPurpose = "test_account_purpose_created";
+      final repository = AccountScreenAppRepository();
+      final account = await repository.addAccount(
+          user, testAccountName, testAccountPurpose);
+      expect(account.user.id, user.id);
+      expect(account.user.userName, user.userName);
+      expect(account.user.password, user.password);
+      expect(account.name, testAccountName);
+      expect(account.purpose, testAccountPurpose);
+    });
   });
 
-  // [テスト] AccountScreenModel
-  test("AccountScreenModelが正しく初期化されていること", () async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
+  group("AccountScreenModel", () {
+    test("AccountScreenModelが正しく初期化されていること", () async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
 
-    final model = AccountScreenModel(
-        TestNotifier(), user, accountScreenRepositoryProvider);
-    expect(model.user.id, user.id);
-    expect(model.user.userName, user.userName);
-    expect(model.user.password, user.password);
-    expect(model.name, "");
-    expect(model.purpose, "");
+      final model = AccountScreenModel(
+          TestNotifier(), user, accountScreenRepositoryProvider);
+      expect(model.user.id, user.id);
+      expect(model.user.userName, user.userName);
+      expect(model.user.password, user.password);
+      expect(model.name, "");
+      expect(model.purpose, "");
+    });
+
+    test("AccountScreenModelのプロパティを更新できていること", () async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      const testAccountName = "test_account_name_created";
+      const testAccountPurpose = "test_account_purpose_created";
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+
+      final model = AccountScreenModel(
+          TestNotifier(), user, accountScreenRepositoryProvider);
+      model.name = testAccountName;
+      model.purpose = testAccountPurpose;
+
+      expect(model.name, testAccountName);
+      expect(model.purpose, testAccountPurpose);
+    });
+
+    test("AccountScreenModelからAccountDataを作成できていること", () async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      const testAccountName = "test_account_name_created";
+      const testAccountPurpose = "test_account_purpose_created";
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+
+      final model = AccountScreenModel(
+          TestNotifier(), user, accountScreenRepositoryProvider);
+      model.name = testAccountName;
+      model.purpose = testAccountPurpose;
+
+      final account = await model.addAccount();
+      expect(account.name, testAccountName);
+      expect(account.purpose, testAccountPurpose);
+    });
   });
 
-  test("AccountScreenModelのプロパティを更新できていること", () async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    const testAccountName = "test_account_name_created";
-    const testAccountPurpose = "test_account_purpose_created";
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
+  group("AccountScreenViewModel", () {
+    test("AccountScreenViewModelが正しく初期化されていること", () {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      expect(viewModel.user.id, user.id);
+      expect(viewModel.user.userName, user.userName);
+      expect(viewModel.user.password, user.password);
+    });
 
-    final model = AccountScreenModel(
-        TestNotifier(), user, accountScreenRepositoryProvider);
-    model.name = testAccountName;
-    model.purpose = testAccountPurpose;
+    testWidgets("AccountScreenViewModelのnameが初期化されていること", (tester) async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.name(context));
+      await tester.pumpWidget(widget);
+      expect(find.text(""), findsOneWidget);
+    });
 
-    expect(model.name, testAccountName);
-    expect(model.purpose, testAccountPurpose);
+    testWidgets("AccountScreenViewModelのpurposeが初期化されていること", (tester) async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.purpose(context));
+      await tester.pumpWidget(widget);
+      expect(find.text(""), findsOneWidget);
+    });
+
+    testWidgets("AccountScreenViewModelのcontentが初期化されていること", (tester) async {
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.content(context));
+      await tester.pumpWidget(widget);
+      expect(find.text(""), findsNWidgets(2));
+    });
+
+    testWidgets("AccountScreenViewModelのbodyが初期化されていること（タブレット）",
+        (tester) async {
+      tester.view.physicalSize = const Size(641, 480);
+      tester.view.devicePixelRatio = 1.0;
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.body(context));
+      await tester.pumpWidget(widget);
+      expect(find.text(""), findsNWidgets(2));
+    });
+
+    testWidgets("AccountScreenViewModelのbodyが初期化されていること（スマホ）", (tester) async {
+      tester.view.physicalSize = const Size(640, 480);
+      tester.view.devicePixelRatio = 1.0;
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.body(context));
+      await tester.pumpWidget(widget);
+      expect(find.text(""), findsNWidgets(2));
+    });
+
+    testWidgets("AccountScreenViewModelのnameの文字列を編集できること", (tester) async {
+      const testAccountName = "test_account_name_created";
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.name(context));
+      await tester.pumpWidget(widget);
+      await tester.enterText(find.byType(TextField), testAccountName);
+      expect(find.text(testAccountName), findsOneWidget);
+    });
+
+    testWidgets("AccountScreenViewModelのpurposeの文字列を編集できること", (tester) async {
+      const testAccountPurpose = "test_account_purpose_created";
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      final widget =
+          TestSkeletonWidget(builder: (context) => viewModel.purpose(context));
+      await tester.pumpWidget(widget);
+      await tester.enterText(find.byType(TextField), testAccountPurpose);
+      expect(find.text(testAccountPurpose), findsOneWidget);
+    });
+
+    testWidgets("AccountScreenViewModelのaddAccountが実行できること", (tester) async {
+      const testAccountName = "test_account_name_created";
+      const testAccountPurpose = "test_account_purpose_created";
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
+      accountScreenRepositoryProvider
+          .overrideRepository(AccountScreenTestRepository());
+
+      final viewModel = AccountScreenViewModel(TestNotifier(), user);
+      await tester.pumpWidget(
+          TestSkeletonWidget(builder: (context) => viewModel.body(context)));
+      await tester.enterText(find.byType(TextField).at(0), testAccountName);
+      await tester.enterText(find.byType(TextField).at(1), testAccountPurpose);
+
+      final account = await viewModel.addAccount();
+      expect(account.user.id, user.id);
+      expect(account.user.userName, user.userName);
+      expect(account.user.password, user.password);
+      expect(account.name, testAccountName);
+      expect(account.purpose, testAccountPurpose);
+    });
   });
 
-  test("AccountScreenModelからAccountDataを作成できていること", () async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    const testAccountName = "test_account_name_created";
-    const testAccountPurpose = "test_account_purpose_created";
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
+  group("AccountScreen", () {
+    testWidgets("AccountScreen", (tester) async {
+      const testAccountName = "test_account_name_created";
+      const testAccountPurpose = "test_account_purpose_created";
+      final user = UserData(
+          id: "test", userName: "testUserName", password: "testPassword");
 
-    final model = AccountScreenModel(
-        TestNotifier(), user, accountScreenRepositoryProvider);
-    model.name = testAccountName;
-    model.purpose = testAccountPurpose;
-
-    final account = await model.addAccount();
-    expect(account.name, testAccountName);
-    expect(account.purpose, testAccountPurpose);
-  });
-
-  // [テスト] AccountScreenViewModel
-  test("AccountScreenViewModelが正しく初期化されていること", () {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    expect(viewModel.user.id, user.id);
-    expect(viewModel.user.userName, user.userName);
-    expect(viewModel.user.password, user.password);
-  });
-
-  testWidgets("AccountScreenViewModelのnameが初期化されていること", (tester) async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.name(context));
-    await tester.pumpWidget(widget);
-    expect(find.text(""), findsOneWidget);
-  });
-
-  testWidgets("AccountScreenViewModelのpurposeが初期化されていること", (tester) async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.purpose(context));
-    await tester.pumpWidget(widget);
-    expect(find.text(""), findsOneWidget);
-  });
-
-  testWidgets("AccountScreenViewModelのcontentが初期化されていること", (tester) async {
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.content(context));
-    await tester.pumpWidget(widget);
-    expect(find.text(""), findsNWidgets(2));
-  });
-
-  testWidgets("AccountScreenViewModelのbodyが初期化されていること（タブレット）", (tester) async {
-    tester.view.physicalSize = const Size(641, 480);
-    tester.view.devicePixelRatio = 1.0;
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.body(context));
-    await tester.pumpWidget(widget);
-    expect(find.text(""), findsNWidgets(2));
-  });
-
-  testWidgets("AccountScreenViewModelのbodyが初期化されていること（スマホ）", (tester) async {
-    tester.view.physicalSize = const Size(640, 480);
-    tester.view.devicePixelRatio = 1.0;
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.body(context));
-    await tester.pumpWidget(widget);
-    expect(find.text(""), findsNWidgets(2));
-  });
-
-  testWidgets("AccountScreenViewModelのnameの文字列を編集できること", (tester) async {
-    const testAccountName = "test_account_name_created";
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.name(context));
-    await tester.pumpWidget(widget);
-    await tester.enterText(find.byType(TextField), testAccountName);
-    expect(find.text(testAccountName), findsOneWidget);
-  });
-
-  testWidgets("AccountScreenViewModelのpurposeの文字列を編集できること", (tester) async {
-    const testAccountPurpose = "test_account_purpose_created";
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    final widget =
-        TestSkeletonWidget(builder: (context) => viewModel.purpose(context));
-    await tester.pumpWidget(widget);
-    await tester.enterText(find.byType(TextField), testAccountPurpose);
-    expect(find.text(testAccountPurpose), findsOneWidget);
-  });
-
-  testWidgets("AccountScreenViewModelのaddAccountが実行できること", (tester) async {
-    const testAccountName = "test_account_name_created";
-    const testAccountPurpose = "test_account_purpose_created";
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-    accountScreenRepositoryProvider
-        .overrideRepository(AccountScreenTestRepository());
-
-    final viewModel = AccountScreenViewModel(TestNotifier(), user);
-    await tester.pumpWidget(
-        TestSkeletonWidget(builder: (context) => viewModel.body(context)));
-    await tester.enterText(find.byType(TextField).at(0), testAccountName);
-    await tester.enterText(find.byType(TextField).at(1), testAccountPurpose);
-
-    final account = await viewModel.addAccount();
-    expect(account.user.id, user.id);
-    expect(account.user.userName, user.userName);
-    expect(account.user.password, user.password);
-    expect(account.name, testAccountName);
-    expect(account.purpose, testAccountPurpose);
-  });
-
-  // [テスト] AccountScreen
-  testWidgets("AccountScreen", (tester) async {
-    const testAccountName = "test_account_name_created";
-    const testAccountPurpose = "test_account_purpose_created";
-    final user = UserData(
-        id: "test", userName: "testUserName", password: "testPassword");
-
-    await tester.pumpWidget(MaterialApp(home: AccountScreen(user: user)));
-    await tester.enterText(find.byType(TextField).at(0), testAccountName);
-    await tester.enterText(find.byType(TextField).at(1), testAccountPurpose);
-    await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpWidget(MaterialApp(home: AccountScreen(user: user)));
+      await tester.enterText(find.byType(TextField).at(0), testAccountName);
+      await tester.enterText(find.byType(TextField).at(1), testAccountPurpose);
+      await tester.tap(find.byType(FloatingActionButton));
+    });
   });
 }
