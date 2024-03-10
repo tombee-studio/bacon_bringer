87b69490cec21cca3cc626bff29da7073432976a
tombee-creator <tomomail1997@gmail.c.om>
2024-03-10 19:08:07 +0900

    TransactionOverviewDataを追加

---
+++ lib/data/transaction_overview_data.dart
@@ -0,0 +1,8 @@
+class TransactionOverviewData {
+  final double monthlyTotalExpense;
+  final double monthlyTotalIncome;
+  final DateTime current;
+
+  TransactionOverviewData(
+      this.monthlyTotalExpense, this.monthlyTotalIncome, this.current);
+}
