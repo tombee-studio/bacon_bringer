556e0f402519e90bf00e89dc6be8263c5e055ea2
tombee-creator <tomomail1997@gmail.c.om>
2024-03-16 17:59:22 +0900

    アカウント作成処理を追加

--- lib/ui/account/view_model/repository/account_screen_app_repository.dart
+++ lib/ui/account/view_model/repository/account_screen_app_repository.dart
@@ -1,12 +1,34 @@
 import 'package:bacon_bringer/data/account_data.dart';
 import 'package:bacon_bringer/data/user_data.dart';
+import 'package:bacon_bringer/database/app_database.dart';
 import 'package:bacon_bringer/repository/account_screen_repository.dart';
+import 'package:drift/drift.dart';
 
 class AccountScreenAppRepository extends AccountScreenRepository {
   @override
   Future<AccountData> addAccount(
       UserData user, String name, String purpose) async {
-    await Future.delayed(const Duration(microseconds: 500));
-    return AccountData(user: user, name: name, purpose: purpose);
+    final db = AppDatabase();
+    final accountId = await db.into(db.dBAccountDataClass).insert(
+        DBAccountDataClassCompanion(
+            user: Value(user.userName),
+            name: Value(name),
+            purpose: Value(purpose)));
+    final row = await (db.select(db.dBAccountDataClass)
+          ..where((tbl) => tbl.id.equals(accountId)))
+        .join([
+      innerJoin(db.dBUserDataClass,
+          db.dBUserDataClass.userName.equalsExp(db.dBAccountDataClass.user))
+    ]).getSingle();
+
+    final dbUser = row.readTable(db.dBUserDataClass);
+    final dbAccount = row.readTable(db.dBAccountDataClass);
+    return AccountData(
+        user: UserData(
+            id: dbUser.id,
+            userName: dbUser.userName,
+            password: dbUser.password),
+        name: dbAccount.name,
+        purpose: dbAccount.purpose);
   }
 }
